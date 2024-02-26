2291f7b43c53116215310a69cbd4100836e67857
ElMiraCorazon <ElMiraCorazon@gmail.com>
2024-02-26 18:44:32 +0200

    Update API documentation

---
+++ main.py
@@ -0,0 +1,16 @@
+# Write your code below this line 👇
+print("Hello world")
+input("What is your Name ?")
+### NEXT #####
+
+# Write your code below this line 👇
+print("Hello world")
+print("My name is Maruf")
+#next
+#input("What is your name ?")
+print("Maruf \nIslam \nSaad")
+#Next
+input("Hello" + " " + input("what is your color ?"))
+print("Maruf" + " Islam")
+ ##
+print("What is yor name ?")
